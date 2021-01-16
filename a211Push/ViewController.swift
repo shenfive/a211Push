9c9e12922cb3415f5d52c74f85631c99fa398104
Shen Jun Wu <shenfive@gmail.com>
2021-01-16 09:44:50 +0800

    push

--- a211Push/ViewController.swift
+++ a211Push/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for theView in view.subviews{
+            if theView.isKind(of: UITextField.classForCoder()){
+                print("yes")
+            }
+        }
+        
+        
+        
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+        NotificationCenter.default.addObserver(self, selector: #selector(shouldRotateObject(_:)), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
-
-
+    
+    @objc func shouldRotateObject(_ sender:Any?){
+        print("test")
+    }
+    
+    
+    
 }
 
